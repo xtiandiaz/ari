51fb22ea90d0213baf19f9d86e5c03d5e0094780
Cristian Díaz <xtndiaz@gmail.com>
2025-06-01 16:51:59 -0500

    Small fix to subtraction generator

--- src/services/operation-generator.ts
+++ src/services/operation-generator.ts
@@ -54,7 +54,7 @@ function generateRandomOperandsForOperator(operator: Operator): number[] {
         const rangeMax = Math.max(100, mult * Math.pow(operatorScore, 3))
         
         const minuend = getRandomInteger(rangeMin, rangeMax)
-        const subtrahend = Math.max(getRandomInteger(rangeMin / 2, minuend), 2)
+        const subtrahend = Math.max(getRandomInteger(rangeMin / 2, minuend) - 2, 2)
         
         return [minuend, subtrahend]
       })()
