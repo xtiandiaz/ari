a26d82986b887735937fa61df979439a9b81bdd1
Cristian Díaz <xtndiaz@gmail.com>
2025-06-03 18:03:00 -0500

    Respect one decimal in percentage. Build app

--- src/services/operation-generator.ts
+++ src/services/operation-generator.ts
@@ -107,7 +107,7 @@ function generateRandomOperandsForOperator(operator: Operator): number[] {
     case Operator.Percent:
       return (() => {        
         const percentAndOperand = getRandomPercentAndOperand(operatorScore, overallLevel)
-        const percentage = Math.floor(percentAndOperand[0] * 100)
+        const percentage = Math.floor(percentAndOperand[0] * 1000) / 10
         
         return [percentAndOperand[1], percentage]
       })()
