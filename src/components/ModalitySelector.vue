485432e6cd87ed897ee9dcc2bfc49541f89c2258
Cristian Díaz <xtndiaz@gmail.com>
2025-07-28 16:00:32 +0300

    Update vueties. Build app

--- src/components/ModalitySelector.vue
+++ src/components/ModalitySelector.vue
@@ -2,9 +2,8 @@
 import { Icon } from '@/assets/design-tokens/iconography';
 import { OperationModality } from '@/models/game';
 import { modalityIcon } from '@/utils/game.utils';
-// import { localizedString } from '@/utils/localization.utils';
+import type { VuetySelectionOption } from '@vueties/components/shared.vm';
 import VuetySegmentedButton from '@vueties/components/buttons/VuetySegmentedButton.vue';
-import type { VuetySegmentedButtonSegment } from '@vueties/components/buttons/view-models'
 
 defineProps<{
   choice?: OperationModality
@@ -14,14 +13,14 @@ const emits = defineEmits<{
   select: [choice?: OperationModality]
 }>()
 
-const segments: VuetySegmentedButtonSegment<OperationModality | undefined>[] = [
+const options: VuetySelectionOption<OperationModality | undefined>[] = [
   OperationModality.Visual, 
   OperationModality.Aural, 
   undefined
 ].map(m => {
   return {
-    key: m,
-    label: '',
+    title: '',
+    value: m,
     icon: m ? modalityIcon(m) : Icon.ArrowShuffle
   }
 })
@@ -31,13 +30,9 @@ const segments: VuetySegmentedButtonSegment<OperationModality | undefined>[] = [
   <div class="wrapper">
     <VuetySegmentedButton 
       :choice="choice"
-      :segments="segments"
+      :options="options"
       @select="(modality) => emits('select', modality)"
     />
-    
-    <!-- <div id="modality-level" class="caption">
-      <strong>{{ localizedString('title-level-abbr') }} 2</strong>
-    </div> -->
   </div>
 </template>
 
@@ -47,11 +42,4 @@ const segments: VuetySegmentedButtonSegment<OperationModality | undefined>[] = [
 .wrapper {
   position: relative;
 }
-
-// #modality-level {
-//   text-transform: uppercase;
-//   transform: translate(-50%, 1.25rem);
-//   @include vs.position(absolute, null, null, 0, 50%);
-//   @include vs.color-attribute('color', vs.$accessory-color);
-// }
 </style>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -3,8 +3,10 @@ import { onBeforeUnmount, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import useRecordsStore from '@/stores/records'
 import useGameStore from '@/stores/game'
+import { Language } from '@/models/localization'
 import { clearScores, clearPersonalBests } from '@/services/records-management'
 import { saveSettings } from '@/services/settings-management'
+import type { VuetySelectionOption } from '@/vueties/components/shared.vm'
 import VuetyForm from '@vueties/components/form/VuetyForm.vue'
 import VuetyFormSection from '@vueties/components/form/VuetyFormSection.vue'
 import VuetyButtonFormRow from '@vueties/components/form/rows/VuetyButtonFormRow.vue'
@@ -16,8 +18,8 @@ const route = useRoute()
 const settings = useGameStore().settings
 const records = useRecordsStore()
 
-const languageSegments = languageSelection.map(l => { 
-  return { key: l, label: localizedStringInLanguage('language-name', l) } 
+const languageOptions: VuetySelectionOption<Language>[] = languageSelection.map(l => { 
+  return { title: localizedStringInLanguage('language-name', l), value: l } 
 })
 
 watch(() => settings.language, () => {
@@ -35,7 +37,7 @@ onBeforeUnmount(() => {
       <VuetyFormSection :title="localizedString('title-language')">
         <VuetySegmentedButtonFormRow
           :choice="settings.language"
-          :segments="languageSegments"
+          :options="languageOptions"
           @select="(lang) => settings.language = lang"
         />
       </VuetyFormSection>
